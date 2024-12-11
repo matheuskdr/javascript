8d6cf66559426b9eaeb799b72b1797f9655b64a2
matheuskdr <matheusgabriellkpo@gmail.com>
2024-12-11 20:34:31 -0300

    +1

---
+++ exercicios/exc1/script.js
@@ -0,0 +1,50 @@
+/*
+Filtra os numeros impares
+
+let numbers = [10, 15, 20, 25, 30, 35];
+
+let oddNumbers = numbers.filter(number => number % 2 !== 0);
+
+console.log(oddNumbers); */
+
+
+/*
+Filtra nomes que contém a letra a.
+
+let names = ['Carlos', 'Ana', 'Bianca', 'Paulo', 'Beatriz', 'jo', 'mel'];
+
+let nameA = names.filter(name => name.includes('a'));
+
+console.log(nameA); */
+
+/*
+Filtra os produtos com o preço abaixo de 500.
+
+let produtos = [
+    {nome: 'Notebook', preco: 2500},
+    {nome: 'Mouse', preco: 50},
+    {nome: 'Teclado', preco: 150},
+    {nome: 'Monitor', preco: 1200},
+];
+
+let produtosBaratos = produtos.filter(produto => produto.preco <= 500);
+
+console.log(produtosBaratos) */
+
+/*
+Filtra palavras com menos de quatro caracteres.
+
+let palavras = ['casa', 'arvore', 'sol', 'lua', 'montanha'];
+
+let menorQuatro = palavras.filter(palavra => palavra.length < 4);
+
+console.log(menorQuatro) */
+
+/*
+Filtra numeros positivos.
+
+let numeros = [-3, -1, 0, 2, 4, 6];
+
+let positive = numeros.filter(numero => numero >= 0);
+
+console.log(positive); */
